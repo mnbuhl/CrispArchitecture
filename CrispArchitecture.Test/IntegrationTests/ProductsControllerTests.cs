94a221a94bf0d6220b9e751719e86457e37e4eb9
Zorxh <Zorxh96@gmail.com>
2021-04-08 11:21:05 +0200

    Finished writing Integration tests for ProductsController. I will not write more Integration Tests for this project as it would mostly be copy and paste.

--- CrispArchitecture.Test/IntegrationTests/ProductsControllerTests.cs
+++ CrispArchitecture.Test/IntegrationTests/ProductsControllerTests.cs
@@ -11,6 +11,8 @@ namespace CrispArchitecture.Test.IntegrationTests
 {
     public class ProductsControllerTests : IntegrationTest
     {
+        private const string Url = BaseUrl + "products/";
+
         [Fact]
         public async Task Get_WithWrongProductId_ReturnsNotFound()
         {
@@ -93,7 +95,29 @@ namespace CrispArchitecture.Test.IntegrationTests
         }
 
         [Fact]
-        public async Task Create_WithWrongPriceType_ReturnsBadRequest()
+        public async Task CreateProduct_WhenUnauthorized_ReturnsUnauthorized()
+        {
+            // Arrange
+            var product = new ProductCommandDto
+            {
+                Name = "Test",
+                Price = 500
+            };
+
+            // Act
+            var response =
+                await TestClient.PostAsJsonAsync(BaseUrl + "products", product);
+
+            var httpResponse = await TestClient.GetAsync(Url);
+            var returnedProducts = await httpResponse.Content.ReadFromJsonAsync<List<ProductResponseDto>>();
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+            returnedProducts.Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task CreateProduct_WithWrongPriceType_ReturnsBadRequest()
         {
             // Arrange
             await AuthenticateAsync();
@@ -104,6 +128,120 @@ namespace CrispArchitecture.Test.IntegrationTests
 
             // Assert
             response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task CreateProduct_SaveToDatabase_ReturnsProductResponse()
+        {
+            // Arrange
+            await AuthenticateAsync();
+
+            var product = new ProductCommandDto
+            {
+                Name = "Test",
+                Price = 500
+            };
+
+            // Act
+            var response =
+                await TestClient.PostAsJsonAsync(BaseUrl + "products", product);
+
+            // Assert
+            var productResponse = await response.Content.ReadFromJsonAsync<ProductResponseDto>();
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+            productResponse.Should().BeEquivalentTo(product);
+        }
+
+        [Fact]
+        public async Task UpdateProduct_WhenNoProductExists_ReturnsNotFound()
+        {
+            // Arrange
+            await AuthenticateAsync();
+
+            // Act
+            var response = await TestClient.PutAsJsonAsync(Url + Guid.NewGuid(),
+                new { name = "test", price = 500 });
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task UpdateProduct_WhenProductExists_ReturnsNoContent()
+        {
+            // Arrange
+            await AuthenticateAsync();
+            var productToUpdate = await CreateProductAsync();
+
+            var newProduct = new ProductCommandDto
+            {
+                Name = "New Test",
+                Price = 520
+            };
+
+            // Act
+            var response = await TestClient.PutAsJsonAsync(Url + productToUpdate.Id, newProduct);
+            bool result = await ContainsEquivalentProduct(productToUpdate.Id, newProduct);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task DeleteProduct_WhenNoProductExists_ReturnsNotFound()
+        {
+            // Arrange
+            await AuthenticateAsync();
+
+            // Act
+            var response = await TestClient.DeleteAsync(Url + Guid.NewGuid());
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task DeleteProduct_WhenUnauthorized_ReturnsUnauthorized()
+        {
+            // Arrange
+
+            // Act
+            var response = await TestClient.DeleteAsync(Url + Guid.NewGuid());
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task DeleteProduct_WhenProductExists_ReturnsNoContent()
+        {
+            // Arrange
+            await AuthenticateAsync();
+            var createdProduct = await CreateProductAsync();
+
+            // Act
+            var response = await TestClient.DeleteAsync(Url + createdProduct.Id);
+            bool result = await ContainsEquivalentProduct(createdProduct.Id,
+                new ProductCommandDto { Name = createdProduct.Name, Price = createdProduct.Price });
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+            result.Should().BeFalse();
+        }
+
+        private async Task<bool> ContainsEquivalentProduct(Guid id, ProductCommandDto product)
+        {
+            var httpResponse = await TestClient.GetAsync(Url + id);
+            var returnedProduct = await httpResponse.Content.ReadFromJsonAsync<ProductResponseDto>();
+
+            if (returnedProduct != null && product.Name != returnedProduct.Name)
+                return false;
+
+            if (returnedProduct != null && Math.Abs(product.Price - returnedProduct.Price) > 0.1)
+                return false;
+
+            return true;
         }
 
         // AUTHENTICATE BEFORE USING
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # CrispArchitecture
 Template / Inspiration for creating an ASP.NET Core 5 Web API following a N-tier architecture
+
 This project is built with Generic Repository in mind.
 
 Project is still being updated.
